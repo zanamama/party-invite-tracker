4d869c33fcddcd51c97a8d452bece1cb1a53006a
zanamama <zmathuthu@yahoo.com>
2025-09-13 10:55:45 -0400

    feat: structure UI with h1 heading using JSX Block

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,9 @@ export default function App() {
   const [guests, setGuests] = useState([]);
 
   return <div>Party Invite Tracker</div>;
+    return (
+    <div>
+      <h1>Party Invite Tracker</h1>
+    </div>
+  );
 }
